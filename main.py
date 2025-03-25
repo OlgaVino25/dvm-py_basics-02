f69188ea511a42e3067797d016a056631af0116b
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-03-25 16:57:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ Neither will happen.
        
 More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...'''"\n", 'ru'))
 
-print (f'78 - {translit(num2words(78), 'ru')}\n'
-	f'15 - {translit(num2words(15), 'ru')}\n'
-	f'3 - {translit(num2words(3), 'ru')}\n'
-	f'40 - {translit(num2words(40), 'ru')}\n'
-	f'8 - {translit(num2words(8), 'ru')}\n')
+print (f'78 - {translit(num2words(78), 'ru')}')
+print (f'15 - {translit(num2words(15), 'ru')}')
+print (f'3 - {translit(num2words(3), 'ru')}')
+printf ('40 - {translit(num2words(40), 'ru')}')
+print (f'8 - {translit(num2words(8), 'ru')}')
