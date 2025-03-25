8a7c8cdfa231fa19f72848d61de7e77d094b2832
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-03-25 16:57:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage 
 print (f'78 - {translit(num2words(78), 'ru')}')
 print (f'15 - {translit(num2words(15), 'ru')}')
 print (f'3 - {translit(num2words(3), 'ru')}')
-printf ('40 - {translit(num2words(40), 'ru')}')
+print (f'40 - {translit(num2words(40), 'ru')}')
 print (f'8 - {translit(num2words(8), 'ru')}')
