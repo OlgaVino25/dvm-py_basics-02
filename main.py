30eacfd00317f32dae53a949bd383d6ac3da5a91
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-03-25 16:49:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ Neither will happen.
        
 More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...'''"\n", 'ru'))
 
-print (f'78 - {translit(num2words(78), 'ru')}\n15 - {translit(num2words(15), 'ru')}\n3 - {translit(num2words(3), 'ru')}')
-print (f'40 - {translit(num2words(40), 'ru')}\n8 - {translit(num2words(8), 'ru')}')
+print (f'78 - {translit(num2words(78), 'ru')}\n'
+	f'15 - {translit(num2words(15), 'ru')}\n'
+	f'3 - {translit(num2words(3), 'ru')}\n'
+	f'40 - {translit(num2words(40), 'ru')}\n'
+	f'8 - {translit(num2words(8), 'ru')}\n')
